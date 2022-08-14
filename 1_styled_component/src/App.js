6538262ae908f441bdcdb0241084fc678a7cffaf
soaelee <soae0923@gmail.com>
2022-08-14 12:26:05 +0900

    styled-component_3: animations and pseudo selectors

--- 1_styled_component/src/App.js
+++ 1_styled_component/src/App.js
@@ -1,55 +1,65 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 const Father = styled.div`
+  width: 100vw;
+  height: 100vh;
   display: flex;
-  margin-bottom: 15px;
+  justify-content: center;
+  align-items: center;
 `;
 
-// props로 설정 가능한 컴포넌트 (Adapting)
+const rotation = keyframes`
+  0% {
+    transform: rotate(0deg);
+    border-radius: 10px;
+  }
+  50% {
+    border-radius: 100px;
+  }
+  100% {
+    transform: rotate(360deg);
+    border-radius: 10px;
+  }
+`;
+
+const notRotation = keyframes`
+  0% {
+    transform: rotate(360deg);
+  }
+  100% {
+    transform: rotate(0deg);
+  }
+`;
+
+const Emoji = styled.span`
+  cursor: pointer;
+  animation: ${notRotation} 1.5s linear infinite;
+  font-size: 36px;
+`;
+
 const Box = styled.div`
-  background-color: ${(props) => props.bgColor ?? "teal"};
+  background-color: ${(props) => props.bgColor ?? "tomato"};
   width: 100px;
   height: 100px;
+  animation: ${rotation} 1.5s linear infinite;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  ${Emoji}:hover {
+    font-size: 45px;
+  }
+  ${Emoji}:active {
+    opacity: 0.5;
+  }
 `;
 
-// 다른 컴포넌트를 확장하는 컴포넌트 (Extending)
-const Circle = styled(Box)`
-  border-radius: 50%;
-`;
-
-const Button = styled.button`
-  color: white;
-  background-color: tomato;
-  border: 0;
-  border-radius: 15px;
-`;
-
-/* 컴포넌트의 속성 설정 */
-const Input = styled.input.attrs({ required: true, minLength: 10 })`
-  background-color: lightpink;
-`;
 const App = () => {
   return (
     <>
       <Father>
-        <Box />
-        <Box bgColor="tomato" />
-        <Circle />
-      </Father>
-      <Father>
-        <Button>LogInButton</Button>
-        {/* as: 다른 태그로 컴포넌트를 사용하고 싶을 때 */}
-        <Button as="a" href="/">
-          LinkButton
-        </Button>
-      </Father>
-      <Father as="header">
-        {/* 컴포넌트의 속성 설정 */}
-        <Input maxLength="15" />
-        <Input />
-        <Input />
-        <Input />
-        <Input />
+        <Box>
+          <Emoji as="p">😊</Emoji>
+        </Box>
       </Father>
     </>
   );
